3c30a8d7f926dab096c3bea4083d1e931b191547
floriane1 <florianedidier@gmail.com>
2020-09-27 15:32:31 -0300

    max characters solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  return Object.keys(chars).reduce((a, b) => (chars[a] > chars[b] ? a : b));
+
+  // alternative
+  //   let max = 0;
+  //   let charMax = "";
+  //   for (let char in chars) {
+  //     if (chars[char] > 0) {
+  //       max = chars[char];
+  //       charMax = char;
+  //     }
+  //   }
+  //   return charMax;
+}
 
 module.exports = maxChar;
