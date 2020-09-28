b4f5e42ab1c0609c98adcd7c76615b8688f1aae6
floriane1 <florianedidier@gmail.com>
2020-09-28 18:42:33 -0300

    count vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // using Regex
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+  // alternative to Regex
+  //   const vowels = ["a", "e", "i", "o", "u"];
+  //   let count = 0;
+  //   for (let letter of str.toLowerCase()) {
+  //     if (vowels.includes(letter)) count++;
+  //   }
+  //   return count;
+}
 
 module.exports = vowels;
