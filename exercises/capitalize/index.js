d1a7811fb09229179d6dd9758c590da114ce9461
floriane1 <florianedidier@gmail.com>
2020-09-28 17:30:58 -0300

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,30 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // using SLICE
+  //   const words = [];
+  //   for (let word of str.split(" ")) {
+  //     words.push(word[0].toUpperCase() + word.slice(1));
+  //   }
+  //   return words.join(" ");
+
+  // alternative
+  let result = str[0].toUpperCase(); // firs character always upcased
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") result += str[i].toUpperCase();
+    else result += str[i];
+  }
+  return result;
+
+  // another alternative
+  //   arr = str.split("");
+  //   newArr = [];
+  //   for (const [index, value] of arr.entries()) {
+  //     if (index === 0 || arr[index - 1] === " ") newArr.push(value.toUpperCase());
+  //     else newArr.push(value);
+  //   }
+  //   return newArr.join("");
+}
 
 module.exports = capitalize;
