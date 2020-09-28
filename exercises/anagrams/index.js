436260001d168dcd64d94be03536c2e23d6be8f4
floriane1 <florianedidier@gmail.com>
2020-09-28 16:58:13 -0300

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// USING SORT METHOD ON ARRAY
+function anagrams(stringA, stringB) {
+  return sortString(stringA) === sortString(stringB);
+}
+
+function sortString(str) {
+  const cleanStr = str.replace(/[^\w]/g, "").toLowerCase();
+  return cleanStr.split("").sort().join("");
+}
+
+// alternative
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     // "in" because aCharMap is an Object
+//     if (aCharMap[char] !== bCharMap[char]) return false;
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   const cleanStr = str.replace(/[^\w]/g, "").toLowerCase();
+//   for (let char of cleanStr) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
 
 module.exports = anagrams;
